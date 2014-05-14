9f110c6e3ab6e299064de4f4d742a62dc366483c
Jake <jakeselig@gmail.com>
2014-05-13 22:02:47 -0600

    Removed swaring :P

--- example.js
+++ example.js
@@ -1,14 +1,14 @@
 var app = new require('express')(); // npm install express
 
 app.get("/", function(req, res) {
-    res.send("<a href='/fuck'>Noobs</a>");
+    res.send( "<a href='/something_else'>Click here for something else!</a>" );
     console.log( "connection from: " + req.ip );
 });
 
-app.get("/fuck", function(req, res) {
+app.get("/something_else", function(req, res) {
     var now = new Date();
-    res.send( "You requested fuck at: " + now );
-    console.log( "fuck at: " + now );
+    res.send( "<h1>You requested something else at: " + now + "</h1>" );
+    console.log( req.ip + " requested something else at: " + now );
 });
 
 app.listen(1337);
