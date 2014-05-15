a366a8e01568ee0ddd70199c60934a3f9737f2fc
Jake <jakeselig@gmail.com>
2014-05-14 21:10:44 -0600

    handlebars using a lot from json

--- example.js
+++ example.js
@@ -1,15 +1,25 @@
-var app = new require('express')(); // npm install express
+var exp = require('express'),
+    hbr = require('express3-handlebars'),
 
-app.get("/", function(req, res) {
-    res.send( "<a href='/something_else'>Click here for something else!</a>" );
-    console.log( "connection from: " + req.ip );
+    app = new exp();
+
+// Rendering .handlebars files
+// By default, looks for them in __dirname + '/views'
+app.engine( 'handlebars', hbr() );
+app.set( 'view engine', 'handlebars' );
+
+// Serving static files
+app.use( '/css', exp.static(__dirname + '/css') );
+
+// Example data
+var data = require('./data/test.json');
+
+// Landing page
+app.get('/', function(req, res) {
+
+    res.render( 'index', data );
+
 });
 
-app.get("/something_else", function(req, res) {
-    var now = new Date();
-    res.send( "<h1>You requested something else at: " + now + "</h1>" );
-    console.log( req.ip + " requested something else at: " + now );
-});
 
 app.listen(1337);
-
